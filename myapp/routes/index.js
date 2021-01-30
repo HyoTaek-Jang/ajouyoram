9b355e57a1fb0b251eaa97f79eeb380eb4968853
HyoTaekJang <hyotaek9812@gmail.com>
2021-01-30 20:03:15 +0900

    ERD 구상 회의, 제작

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,11 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.post("/", (req, res) => {
+  let data = req.body;
+  console.log(data);
+  let str = JSON.stringify(data);
+  res.send(str);
+});
+
 module.exports = router;
